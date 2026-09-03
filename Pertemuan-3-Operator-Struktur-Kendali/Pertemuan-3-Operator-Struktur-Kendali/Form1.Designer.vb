586d3c0a94bb3efda5f2cce59950f07dbb7d305c
nadyalahfah <nadyalahfah171@gmail.com>
2026-09-03 15:47:22 +0700

    Pertemuan 3 Operator Struktur Kendali

---
+++ Pertemuan-3-Operator-Struktur-Kendali/Pertemuan-3-Operator-Struktur-Kendali/Form1.Designer.vb
@@ -0,0 +1,75 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        picImage = New PictureBox()
+        btnInput = New Button()
+        txtNilai = New TextBox()
+        CType(picImage, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' picImage
+        ' 
+        picImage.Location = New Point(157, 80)
+        picImage.Name = "picImage"
+        picImage.Size = New Size(100, 100)
+        picImage.SizeMode = PictureBoxSizeMode.Zoom
+        picImage.TabIndex = 0
+        picImage.TabStop = False
+        ' 
+        ' btnInput
+        ' 
+        btnInput.Location = New Point(135, 246)
+        btnInput.Name = "btnInput"
+        btnInput.Size = New Size(145, 29)
+        btnInput.TabIndex = 1
+        btnInput.Text = "Input"
+        btnInput.UseVisualStyleBackColor = True
+        ' 
+        ' txtNilai
+        ' 
+        txtNilai.Location = New Point(135, 198)
+        txtNilai.Name = "txtNilai"
+        txtNilai.Size = New Size(145, 27)
+        txtNilai.TabIndex = 2
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(txtNilai)
+        Controls.Add(btnInput)
+        Controls.Add(picImage)
+        Name = "Form1"
+        Text = "Form1"
+        CType(picImage, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents picImage As PictureBox
+    Friend WithEvents btnInput As Button
+    Friend WithEvents txtNilai As TextBox
+
+End Class
